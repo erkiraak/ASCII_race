6f556e44b539e0ae87cc3d01934f2599c8bdafb3
Erki <erkiraak@gmail.com>
2022-02-01 18:43:35 +0800

    V2

--- ASCII_race.py
+++ ASCII_race.py
@@ -1,7 +1,7 @@
 import time
 import race_manager
 import result
-
+# v2
 
 input_command = ""
 race_manager = race_manager.RaceManager()
@@ -18,15 +18,15 @@ def race():
     print(f"Top 3 finishers:\n {results.get_results(race_manager.get_results())}\n")
 
 
-while input_command != "exit":
+while input_command != "3":
 
-    while input_command not in ["race", "history", "exit"]:
-        input_command = input('Type "Race" to have a race\n'
-                              'Type "History" to see race history\n'
-                              'Type "Exit" to exit program\n').lower()
-    if input_command == "race":
+    while input_command not in ["1", "2", "3"]:
+        input_command = input('Type "1" to have a race\n'
+                              'Type "2" to see race history\n'
+                              'Type "3" to exit program\n').lower()
+    if input_command == "1":
         race()
         input_command = ""
-    elif input_command == "history":
+    elif input_command == "2":
         results.print_race_results()
         input_command = ""
